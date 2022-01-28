bbdb47a3109ad43a1110c82d2410837a6ad9d5c6
comet1992611 <comet.k1992611@gmail.com>
2022-01-28 22:28:44 +0700

    update the website

--- src/pages/artnewyorkclan.jsx
+++ src/pages/artnewyorkclan.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import SmoothScroll from "smooth-scroll";
@@ -71,7 +71,6 @@ const Artnewyorkclan = () => {
     setIsShowDwarfSolders(false);
     setIsShowDwarfCapos(false);
     setIsShowBosses(true);
-    console.log("show bosses");
     return;
   };
 
@@ -79,7 +78,6 @@ const Artnewyorkclan = () => {
     setIsShowDwarfSolders(false);
     setIsShowDwarfCapos(true);
     setIsShowBosses(false);
-    console.log("show DwarfCapos");
     return;
   };
 
@@ -87,7 +85,13 @@ const Artnewyorkclan = () => {
     setIsShowDwarfSolders(true);
     setIsShowDwarfCapos(false);
     setIsShowBosses(false);
-    console.log("show DwarfSoliders");
+    return;
+  };
+
+  const showMainOptions = () => {
+    setIsShowDwarfSolders(false);
+    setIsShowDwarfCapos(false);
+    setIsShowBosses(false);
     return;
   };
 
@@ -109,6 +113,9 @@ const Artnewyorkclan = () => {
 
   return (
     <div className="col-md-8 col-sm-8">
+      <div className='row text-right'>
+        <button type='button' className="btn btn-art-back" onClick={() => showMainOptions()}><i className="fa fa-long-arrow-left"></i> Back</button>
+      </div>
       {
         !isShowBosses && !isShowDwarfCapos && !isShowDwarfSolders &&
         <div className="row">
@@ -126,7 +133,7 @@ const Artnewyorkclan = () => {
             </Card>
           </div>
           <div className="col-md-6 col-sm-6">
-            <Card>
+            <Card onClick={() => showBosses()}>
               <Card.Img variant="top" src={bossImg} style={{ width: '100%' }} />
               <Card.Body>
                 <Card.Title>Bosses</Card.Title>
@@ -139,7 +146,7 @@ const Artnewyorkclan = () => {
             </Card>
           </div>
           <div className="col-md-6 col-sm-6">
-            <Card>
+            <Card onClick={() => showDwarfCapos()}>
               <Card.Img variant="top" src={dwarfCaposImg} style={{ width: '100%' }} />
               <Card.Body>
                 <Card.Title>DwarfCapos</Card.Title>
@@ -152,7 +159,7 @@ const Artnewyorkclan = () => {
             </Card>
           </div>
           <div className="col-md-6 col-sm-6">
-            <Card>
+            <Card onClick={() => showDwarfSoliders()}>
               <Card.Img variant="top" src={dwarfSolderImg} style={{ width: '100%' }} />
               <Card.Body>
                 <Card.Title>DwarfSoldiers</Card.Title>
