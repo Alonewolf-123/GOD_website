720c6116892c28dea1b13569fe0bef1542a9fdf6
comet1992611 <comet.k1992611@gmail.com>
2022-01-28 21:43:58 +0700

    update the website

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,32 @@
-import { useState } from "react";
+
+import { useState, useEffect } from "react";
 import discordImg from "../assets/img/social/icons8-discord-64.png";
 import discordHoverImg from "../assets/img/social/icons8-discord-64-hover.png";
-import image from "../assets/img/intro-bg.png";
 
 export const Header = (props) => {
   const [isHover, setIsHover] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  function handleWindowSizeChange() {
+    setIsMobile(window.innerWidth <= 768);
+  }
+  useEffect(() => {
+      window.addEventListener('resize', handleWindowSizeChange);
+      return () => {
+          window.removeEventListener('resize', handleWindowSizeChange);
+      }
+  }, []);
+  
   
   return (
     <header id='header'>
       <div className='intro'>
-        <img src={image} alt=""></img>
-        <div className='col-md-8 col-md-offset-2 intro-text'>
-          <a href='/mint' className='btn btn-custom btn-lg page-scroll' onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>Join Discord<img src={isHover ? discordHoverImg : discordImg} alt=""></img></a>{' '}
+        <div className='overlay'>
+          <div className='container'>
+            <div className='col-md-8 col-md-offset-2 intro-text'>
+              <a href='/mint' className='btn btn-custom btn-lg page-scroll' onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>Join Discord<img src={isHover || isMobile ? discordHoverImg : discordImg} alt=""></img></a>{' '}
+            </div>
+          </div>
         </div>
       </div>
     </header>
