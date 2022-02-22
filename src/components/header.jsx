68d034d540f11f62b11f57990ba93195c792a4d3
LiuWanJun1988 <99682718+LiuWanJun1988@users.noreply.github.com>
2022-02-22 22:44:51 +0700

    Update the GOD website

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,9 @@
 
 import { useState, useEffect } from "react";
-import bannerImg from "../assets/img/website_main_image-01.png";
+// import bannerImg from "../assets/img/website_main_image-01.png";
 import bannerVideoDesktop from "../assets/img/banner_video_desktop.mp4";
-import bannerVideoMobile from "../assets/img/banner_video_mobile.mp4";
-import bannerMobileImg from "../assets/img/banner_mobile.png";
+// import bannerVideoMobile from "../assets/img/banner_video_mobile.mp4";
+import bannerMobileImg from "../assets/img/banner_mobile.gif";
 // import discordImg from "../assets/img/social/icons8-discord-64.png";
 // import discordHoverImg from "../assets/img/social/icons8-discord-64-hover.png";
 
@@ -29,10 +29,11 @@ export const Header = (props) => {
           <div className='container'>
             <div className='col-md-8 col-md-offset-2 intro-text'> */}
         {/* <img src={ isMobile ? bannerMobileImg : bannerImg} alt=""></img> */}
-        {/* <img className="bannerTextImg" src={bannerTextImg} alt=""></img> */}
-        <video autoPlay muted loop id="video">
-          <source src={ isMobile ? bannerVideoMobile : bannerVideoDesktop} type="video/mp4" />
-        </video>
+
+        {isMobile ? <img src={bannerMobileImg} alt=""></img>
+          : <video autoPlay muted loop id="video">
+            <source src={bannerVideoDesktop} type="video/mp4" />
+          </video>}
         {/* <a href='/mint' className='btn btn-custom btn-lg page-scroll' onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>Join Discord<img src={isHover || isMobile ? discordHoverImg : discordImg} alt=""></img></a>{' '} */}
         <a href='/mint' className='btn btn-custom btn-lg page-scroll'>JOIN DISCORD</a>{' '}
         {/* </div>
