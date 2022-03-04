9ebe43ff7d7c3c8113c42fc41e20cbf00f431186
LiuWanJun1988 <99682718+LiuWanJun1988@users.noreply.github.com>
2022-03-04 22:20:15 +0700

    Update the GOD website

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,10 @@
 
 import { useState, useEffect } from "react";
 // import bannerImg from "../assets/img/website_main_image-01.png";
-import bannerVideoDesktop from "../assets/img/banner_video_desktop.mp4";
+// import bannerVideoDesktop from "../assets/img/banner_video_desktop.mp4";
 // import bannerVideoMobile from "../assets/img/banner_video_mobile.mp4";
 import bannerMobileImg from "../assets/img/banner_mobile.gif";
+import bannerDesktopImg from "../assets/img/banner_desktop.gif";
 // import discordImg from "../assets/img/social/icons8-discord-64.png";
 // import discordHoverImg from "../assets/img/social/icons8-discord-64-hover.png";
 
@@ -28,12 +29,12 @@ export const Header = (props) => {
         {/* <div className='overlay'>
           <div className='container'>
             <div className='col-md-8 col-md-offset-2 intro-text'> */}
-        {/* <img src={ isMobile ? bannerMobileImg : bannerImg} alt=""></img> */}
+        <img src={ isMobile ? bannerMobileImg : bannerDesktopImg} alt="" onLoad={props.onLoad}></img>
 
-        {isMobile ? <img src={bannerMobileImg} alt="" onLoad={props.onLoad}></img>
+        {/* {isMobile ? <img src={bannerMobileImg} alt="" onLoad={props.onLoad}></img>
           : <video autoPlay muted loop id="video" onLoadedData={props.onLoad}>
             <source src={bannerVideoDesktop} type="video/mp4" />
-          </video>}
+          </video>} */}
         {/* <a href='/mint' className='btn btn-custom btn-lg page-scroll' onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>Join Discord<img src={isHover || isMobile ? discordHoverImg : discordImg} alt=""></img></a>{' '} */}
         <a href='/mint' className='btn btn-custom btn-lg page-scroll'>JOIN DISCORD</a>{' '}
         {/* </div>
