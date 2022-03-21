8a828ec5e09eb8c77b5ccb4daa6d5fbb36fac7b3
LiuWanJun1988 <99682718+LiuWanJun1988@users.noreply.github.com>
2022-03-21 21:38:30 +0700

    GOD website

--- src/components/team.jsx
+++ src/components/team.jsx
@@ -4,9 +4,9 @@ import member_2 from '../assets/img/team/02.png'
 import member_3 from '../assets/img/team/03.png'
 import member_4 from '../assets/img/team/04.png'
 import member_5 from '../assets/img/team/05.png'
-import member_6 from '../assets/img/team/06.png'
+// import member_6 from '../assets/img/team/06.png'
 import member_7 from '../assets/img/team/07.png'
-import member_8 from '../assets/img/team/08.png'
+// import member_8 from '../assets/img/team/08.png'
 import linkedin_icon from '../assets/img/social/Icon-Linkedin-In-circle.png'
 import twitter_icon from '../assets/img/social/Icon-Twitter-In-circle.png'
 import linkedin_icon_hovor from '../assets/img/social/Icon-Linkedin-In-circle-hovor.png'
@@ -16,7 +16,7 @@ export const Team = (props) => {
 
   const [isHover, setIsHover] = useState([false, false, false, false, false, false, false, false]);
 
-  const imgs = [member_1, member_2, member_3, member_4, member_5, member_6, member_7, member_8];
+  const imgs = [member_1, member_2, member_3, member_5, member_7, member_4];
 
   const onMouseEnter = (index) => {
     let isHover = [false, false, false, false, false, false, false, false ];
@@ -39,7 +39,7 @@ export const Team = (props) => {
         <div className='row'>
           {props.data
             ? props.data.map((item, i) => (
-              <div key={`${item.name}-${i}`} className='col-md-3 col-sm-6 team'>
+              <div key={`${item.name}-${i}`} className='col-md-4 col-sm-6 team'>
                 <div className='thumbnail'>
                   <img src={imgs[i]} alt='...' className='team-img' />
                   <a href={item.sns} onMouseEnter={() => onMouseEnter(i)} onMouseLeave={() => onMouseLeave()}><img src={isHover[i] ? (i == 1 ? twitter_icon_hovor : linkedin_icon_hovor) : (i == 1 ? twitter_icon : linkedin_icon)} alt='...' className='team-icon-img' /></a>
