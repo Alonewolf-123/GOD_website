00a612a9d26ebfd3f5b812282ad3578b74e54207
LiuWanJun1988 <99682718+LiuWanJun1988@users.noreply.github.com>
2022-04-09 19:07:02 +0700

    GOD website

--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -46,10 +46,10 @@ export const Summary = (props) => {
           </table> */}
         </div>
         <div className="col-md-7 div-center" style={{marginTop: "60px"}}>
-          <h4 style={{fontWeight: "bold"}}>CONTRACT ADDRESSES</h4>
-          <p style={{textAlign: "center"}}><a style={{fontWeight: "400"}} href="https://rinkeby.etherscan.io/address/0xFA3a0a169c0fD067086E1f3eEEa6c8ebC2e11b69">{isMobile ? "0xFA3a0a...e11b69" : "0xFA3a0a169c0fD067086E1f3eEEa6c8ebC2e11b69"}</a></p>
-          <p style={{textAlign: "center"}}><a style={{fontWeight: "400"}} href="https://rinkeby.etherscan.io/address/0x25c475D80Bb8688Cbc9AB2d5720d4F92bBe63C5e">{isMobile ? "0x25c475...e63C5e" : "0x25c475D80Bb8688Cbc9AB2d5720d4F92bBe63C5e"}</a></p>
-          <p style={{textAlign: "center"}}><a style={{fontWeight: "400"}} href="https://rinkeby.etherscan.io/address/0xfb01bc8634E565Cd013ac0442b5B39Ed80BEB149">{isMobile ? "0xfb01bc...BEB149" : "0xfb01bc8634E565Cd013ac0442b5B39Ed80BEB149"}</a></p>
+          <h4>CONTRACT ADDRESSES</h4>
+          <p className="contrac-address"><a style={{fontWeight: "400"}} href="https://rinkeby.etherscan.io/address/0xFA3a0a169c0fD067086E1f3eEEa6c8ebC2e11b69">{isMobile ? "0xFA3a0a...e11b69" : "0xFA3a0a169c0fD067086E1f3eEEa6c8ebC2e11b69"}</a></p>
+          <p className="contrac-address"><a style={{fontWeight: "400"}} href="https://rinkeby.etherscan.io/address/0x25c475D80Bb8688Cbc9AB2d5720d4F92bBe63C5e">{isMobile ? "0x25c475...e63C5e" : "0x25c475D80Bb8688Cbc9AB2d5720d4F92bBe63C5e"}</a></p>
+          <p className="contrac-address"><a style={{fontWeight: "400"}} href="https://rinkeby.etherscan.io/address/0xfb01bc8634E565Cd013ac0442b5B39Ed80BEB149">{isMobile ? "0xfb01bc...BEB149" : "0xfb01bc8634E565Cd013ac0442b5B39Ed80BEB149"}</a></p>
         </div >
       </div>
       {/* <div className={isMobile ? "col-md-4 div-center" : "side_banner_video"}>
